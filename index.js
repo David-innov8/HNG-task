de56d5022db52f69fb79765f83101701b015735a
David-innov8 <115226033+David-innov8@users.noreply.github.com>
2023-09-09 16:55:16 +0100

    finished the js and styling

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fri
 const currentDate = new Date();
 const currentDay = daysOfWeek[currentDate.getUTCDay()];
 
-// Get the time components
+
 const hours = currentDate.getUTCHours().toString().padStart(2, '0');
 const minutes = currentDate.getUTCMinutes().toString().padStart(2, '0');
 const seconds = currentDate.getUTCSeconds().toString().padStart(2, '0');
 
-// Format the time
+
 const currentTime = `${hours}:${minutes}:${seconds}`;
 
-// Update the elements
+
 document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDay;
 document.querySelector('[data-testid="currentUTCTime"]').textContent = currentTime;
